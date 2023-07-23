1ca5f4bf5025ee34196f8e700ab26e47cbb79e54
kidanetes <kidanemaryamtesfaye@gmail.com>
2023-07-23 13:54:09 -0700

    adding a file which contain big O notation of buble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort an array using
+ * bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of an array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, status, tmp;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		status = 0;
+		for (j = 0; j < ((int)size) - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				status = 1;
+				print_array(array, size);
+			}
+		}
+		if (status == 0)
+			break;
+	}
+}
